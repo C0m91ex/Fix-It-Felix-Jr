b6cc76d0037c9ccf9464b433758eb8c6497cf9ba
C0m91ex <jtorr124@ucsc.edu>
2024-02-28 20:35:41 -0800

    adding logo asset

--- src/main.js
+++ src/main.js
@@ -11,7 +11,11 @@ let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    pixelArt: true,
     scene: [ Menu, Play ]
 }
 
 const game = new Phaser.Game(config)
+
+const centerX = game.config.width / 2
+const centerY = game.config.height / 2
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -4,16 +4,19 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('logo', './assets/Fix-It_Felix_Jr._Logo.png')
     }
 
     create() {
-        this.keySpace = this.input.keyboard.addKey(Phaser.Inpu.Keyboard.Keycodes.SPACE)
+       // this.keySpace = this.input.keyboard.addKey(Phaser.Inpu.Keyboard.Keycodes.SPACE)
+
+        logo = this.add.image(centerX, centerY, 'logo').setOrigin(0.5)
+        logo.setScale(.5)
     }
 
     update() {
-        if(this.keySpace.isDown) {
-            this.scene.start("playScene")
-        }
+        //if(this.keySpace.isDown) {
+            //this.scene.start("playScene")
+        //}
     }
 }
